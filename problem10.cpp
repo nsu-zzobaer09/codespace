a58b5c7e2f68c02968936ce4a4dff72a436fcc34
M F Khan Zobaer <khan.zobaer.253@northsouth.edu>
2026-07-19 09:33:07 +0000

    problem10

---
+++ problem10.cpp
@@ -0,0 +1,48 @@
+/*
+    https://vjudge.net/contest/826394#problem/J
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n; cin>>n;
+    char c;
+    string s = "0000000000";
+    
+    string assignment; cin>>assignment;
+
+    int start = 0;
+    int end = 9;
+
+    for (int i = 0; i < n; i++)
+    {
+        char c = assignment[i];
+        if(c=='L'){
+            while (s[start]!='0')
+            {
+                start++;
+            }
+            
+            s[start] = '1';
+            start++;
+        }else if(c=='R'){
+            while (s[end]!='0')
+            {
+                start--;
+            }
+            
+            s[end] = '1';
+            end--;
+        }else{
+            int ii = c-'0';
+            s[ii] = '0';
+            if(abs(ii-start)<abs(ii-end)) start = ii;
+            else end = ii;
+        } 
+    }
+    cout<<s<<endl;
+}
